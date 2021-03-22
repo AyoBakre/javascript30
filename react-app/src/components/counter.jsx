064a5d65c7f95b4698acc73fe9ad802a08b09c08
AyoBakre <ayobakre@gmail.com>
2021-03-22 22:07:53 +0100

    react

---
+++ react-app/src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+	state = {
+		count: 0,
+	};
+
+	render() {
+		return (
+			<div>
+				<span className={this.getBadgeClasses()}>
+					{this.formatCount()}
+				</span>
+				<button
+					onClick={ () => this.handleIncrement(product)}
+					className="btn btn-secondary btn-sm"
+				>
+					Increment
+				</button>
+			</div>
+		);
+	}
+
+	handleIncrement = product => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "Zero" : count;
+	}
+}
+
+export default Counter;
